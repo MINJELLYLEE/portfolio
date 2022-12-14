836751b0f7ef369b46287995e1a94ffd2f24ea3a
MINJELLYLEE <amkids12345@gmail.com>
2022-12-14 15:02:17 +0900

    2022.12.14.15:02

--- css_java/common.js
+++ css_java/common.js
@@ -1,3 +1,13 @@
+$(function () {
+  $(".btn>button").click(function () {
+    let btn_index = $(this).index();
+    console.log(btn_index);
+    $(".btn>button, .sec2_con>article").removeClass("on");
+    $(this).addClass("on");
+    $(".sec2_con>article").eq(btn_index).addClass("on");
+  });
+});
+
 var swiper = new Swiper(".slide1", {
   pagination: {
     el: ".pg1",
